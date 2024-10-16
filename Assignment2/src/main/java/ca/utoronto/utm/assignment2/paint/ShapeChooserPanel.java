d04d9af9521a85cfdd71e07517174b3351d03d0e
Jiaye Tian <aki.tian@mail.utoronto.ca>
2024-10-15 21:05:21 -0400

    BUG1.002 UI embellish

--- Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/ShapeChooserPanel.java
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/ShapeChooserPanel.java
@@ -8,16 +8,29 @@ public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEv
 
         private View view;
 
+        // css properties for button highlighting
+        private final String SELECTED = "-fx-background-color: rgba(255, 255, 0, 0.2);" +
+                "-fx-border-color: rgba(0, 0, 0, 0.2);" +
+                "-fx-border-radius: 3px;";
+
+        private final String UNSELECTED = "-fx-background-color: rgba(225, 225, 225, 1);" +
+                "-fx-border-color: rgba(0, 0, 0, 0.2);" +
+                "-fx-border-radius: 3px;";
+
         public ShapeChooserPanel(View view) {
 
                 this.view = view;
 
                 String[] buttonLabels = { "●", "▬", "■", "~", "〽" };
 
+                // adding gap between elements
+                this.setVgap(5.0);
+
                 int row = 0;
                 for (String label : buttonLabels) {
                         Button button = new Button(label);
                         button.setMinWidth(100);
+                        button.setStyle(UNSELECTED);
                         this.add(button, 0, row);
                         row++;
                         button.setOnAction(this);
@@ -34,9 +47,9 @@ public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEv
                 for (Object o : this.getChildren()) {
                         Button b = (Button) o;
                         if (b.getText().equals(command)) {
-                                b.setStyle("-fx-background-color: yellow;");
+                                b.setStyle(SELECTED);
                         } else {
-                                b.setStyle(null);
+                                b.setStyle(UNSELECTED);
                         }
                 }
         }
