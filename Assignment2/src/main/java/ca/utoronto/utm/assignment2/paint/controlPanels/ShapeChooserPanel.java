2d006011168d00c237e4fcf3f1d4cd9dcf6bd661
huaethan <ethan.hua@mail.utoronto.ca>
2024-11-07 15:43:19 -0500

    reorder the appearance of shapes in ShapeChooserPanel

--- Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/controlPanels/ShapeChooserPanel.java
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/controlPanels/ShapeChooserPanel.java
@@ -15,8 +15,8 @@ public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEv
 
         int row = 0;
         // Ethan: using buttonLabels as mode breaks the code
-        String[] labels= {"●", "▬", "■", "▲", "⬬", "~", "〽", "😎", "⁺₊✧"};
-        String[] commands = {"Circle", "Rectangle", "Square", "Triangle", "Oval", "Squiggle", "Polyline", "PrecisionEraser", "SmartShape"};
+        String[] labels= {"●", "▬", "■", "▲", "⬬", "~", "〽", "⁺₊✧", "😎"};
+        String[] commands = {"Circle", "Rectangle", "Square", "Triangle", "Oval", "Squiggle", "Polyline", "SmartShape", "PrecisionEraser"};
         for (int i = 0; i < labels.length; i++) {
             ShapeChooserPanelButton button = new ShapeChooserPanelButton(labels[i] + " " + commands[i], commands[i], false);
             if (labels[i].equals("●")) button.setSelected(true);
