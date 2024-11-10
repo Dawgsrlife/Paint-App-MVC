89df6bdd57c850209a78f262eda1657668415a99
mengale1 <alex.meng@mail.utoronto.ca>
2024-11-10 15:28:50 -0500

    clean Polyline.java for re-implementation

--- Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/Polyline.java
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/Polyline.java
@@ -3,11 +3,9 @@ package ca.utoronto.utm.assignment2.paint;
 import java.util.ArrayList;
 
 public class Polyline extends Squiggle {
-    private boolean isFinalized;
 
     public Polyline(Point point, PaintProperties pp, ArrayList<Point> path) {
         super(point, pp, path);
         setType("Polyline");
-        isFinalized = false;
     }
 }
