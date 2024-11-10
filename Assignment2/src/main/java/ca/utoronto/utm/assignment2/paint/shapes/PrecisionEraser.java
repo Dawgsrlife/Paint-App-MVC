1e475f25c5feb06fca96e13d5bfd2e21cd9f97ca
tianji61 <aki.tian@mail.utoronto.ca>
2024-11-10 18:29:58 -0500

    PR US4.027 accepted

--- Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/shapes/PrecisionEraser.java
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/shapes/PrecisionEraser.java
@@ -2,30 +2,14 @@ package ca.utoronto.utm.assignment2.paint.shapes;
 
 import ca.utoronto.utm.assignment2.paint.PaintProperties;
 import ca.utoronto.utm.assignment2.paint.PaintView;
-import javafx.scene.canvas.GraphicsContext;
 
 import java.util.ArrayList;
 
 public class PrecisionEraser extends Squiggle {
-    private double eraserSize = 10.0;
 
     public PrecisionEraser(Point point, PaintProperties pp, ArrayList<Point> path) {
         super(point, pp, path);
+        pp.setStrokeColor(PaintView.BACKGROUND_COLOR);
         setType("PrecisionEraser");
-    }
-
-    @Override
-    public void paint(GraphicsContext g2d) {
-        g2d.setStroke(PaintView.BACKGROUND_COLOR);
-
-        for (int i = 0; i < getPoints().size() - 1; i++) {
-            Point p1 = getPoints().get(i);
-            Point p2 = getPoints().get(i + 1);
-            g2d.strokeLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
-        }
-    }
-
-    public void setEraserSize(double eraserSize) {
-        this.eraserSize = eraserSize;
     }
 }
